9acd83770f5e406b96118676e461c9a2a739a853
rugk <rugk+git@posteo.de>
2025-03-30 16:24:32 +0200

    style: add JSDoc to store.ts
    Maybe you like the JSDoc, too? Builds upon https://github.com/missive/emoji-mart/pull/989

--- packages/emoji-mart/src/helpers/store.ts
+++ packages/emoji-mart/src/helpers/store.ts
@@ -1,9 +1,19 @@
+/**
+ * Retrieves a stored value by key.
+ * @param key - The key of the stored value.
+ * @returns The retrieved value.
+ */
 function set(key: string, value: any) {
   try {
     window.localStorage[`emoji-mart.${key}`] = JSON.stringify(value)
   } catch (error) {}
 }
 
+/**
+ * Stores a value under a specific key.
+ * @param key - The key to store the value under.
+ * @param value - The value to store.
+ */
 function get(key: string): any {
   try {
     const value = window.localStorage[`emoji-mart.${key}`]
